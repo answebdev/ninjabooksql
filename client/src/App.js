a93b587248e8ebb677107fdf9b7f94a7395a6e4b
ANS <36783010+answebdev@users.noreply.github.com>
2021-09-19 10:08:37 -0700

    add BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-function App() {
+import BookList from './components/BookList';
+
+const App = () => {
   return (
     <div id='main'>
       <h1>Ninja Books</h1>
+      <BookList />
     </div>
   );
-}
+};
 
 export default App;
