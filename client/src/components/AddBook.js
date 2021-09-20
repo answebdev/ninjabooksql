47687621ef6ef6cfe48caaa49d262aa06a55f55d
ANS <36783010+answebdev@users.noreply.github.com>
2021-09-20 15:10:03 -0700

    add state to AddBook component

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { getAuthorsQuery } from '../queries/queries';
 
@@ -12,36 +12,61 @@ import { getAuthorsQuery } from '../queries/queries';
 //   }
 // `;
 
+const displayAuthors = (loading, data) => {
+  if (loading) {
+    return <option disabled>Loading authors...</option>;
+  } else {
+    return data.authors.map((author) => {
+      return (
+        <option key={author.id} value={author.id}>
+          {author.name}
+        </option>
+      );
+    });
+  }
+};
+
 const AddBook = () => {
-  const { loading, error, data } = useQuery(getAuthorsQuery);
-  if (loading) return <option disabled>Loading Authors...</option>;
-  if (error) return `Error! ${error.message}`;
-  console.log(data);
+  //   const { loading, error, data } = useQuery(getAuthorsQuery);
+  //   if (loading) return <option disabled>Loading Authors...</option>;
+  //   if (error) return `Error! ${error.message}`;
+  //   console.log(data);
+
+  const [name, setName] = useState('');
+  const [genre, setGenre] = useState('');
+  const [authorId, setAuthorId] = useState('');
+  const { loading, data } = useQuery(getAuthorsQuery);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, genre, authorId);
+  };
 
   return (
-    <form id='add-book'>
+    <form id='add-book' onSubmit={handleSubmit}>
       <div className='field'>
         <label>Book name:</label>
-        <input type='text' />
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
-
       <div className='field'>
         <label>Genre:</label>
-        <input type='text' />
+        <input
+          type='text'
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+        />
       </div>
-
       <div className='field'>
         <label>Author:</label>
-        <select>
-          <option>Select Author</option>
-          {data.authors.map((author) => (
-            <option key={author.id} value={author.id}>
-              {author.name}
-            </option>
-          ))}
+        <select value={authorId} onChange={(e) => setAuthorId(e.target.value)}>
+          <option>Select author</option>
+          {displayAuthors(loading, data)}
         </select>
       </div>
-
       <button>+</button>
     </form>
   );
