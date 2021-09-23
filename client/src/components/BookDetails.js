0ac8363b40b1a5432c6ae82b51cd7ad2b1e6ef80
ANS <36783010+answebdev@users.noreply.github.com>
2021-09-22 20:02:33 -0700

    add query to get book details

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,12 +1,37 @@
-import React from 'react';
 import { useQuery } from '@apollo/client';
- import { getBookQuery } from '../queries/queries';
+import { getBookQuery } from '../queries/queries';
 
-const BookDetails = () => {
+const displayBookDetails = (loading, data, error) => {
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong..</p>;
+  if (data.book) {
+    return (
+      <div>
+        <h2>{data.book.name}</h2>
+        <p>{data.book.genre}</p>
+        <p>{data.book.author.name}</p>
+        <p>All books by this author:</p>
+        <ul className='other-books'>
+          {data.book.author.books.map((item) => {
+            return <li key={item.id}>{item.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return <div>No book selected...</div>;
+  }
+};
+
+const BookDetails = (props) => {
+  const { loading, data, error } = useQuery(getBookQuery, {
+    variables: {
+      id: props.bookId,
+    },
+  });
+  // console.log(data)
   return (
-    <div id='book-details'>
-      <p>Output book details here</p>
-    </div>
+    <div id='book-details'>{displayBookDetails(loading, data, error)}</div>
   );
 };
 
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,51 +1,39 @@
-import React from 'react';
 import { useQuery } from '@apollo/client';
 import { getBooksQuery } from '../queries/queries';
+import { useState } from 'react';
+// import { useState } from 'react';
+
 import BookDetails from './BookDetails';
 
-// Move query to 'queries.js' file
-// // STEP 1: Create query: Store query in a function
-// // Note that this query looks like the queries made in the GraphiQL tool.
-// const getBooksQuery = gql`
-//   {
-//     books {
-//       name
-//       id
-//     }
-//   }
-// `;
+const DisplayBooks = ({ loading, error, data, selectBook }) => {
+  if (loading) return <p>Loading....</p>;
+  if (error) return <p>Something went wrong</p>;
+  return data.books.map((book) => {
+    return (
+      <li key={book.id} onClick={() => selectBook(book.id)}>
+        {book.name}
+      </li>
+    );
+  });
+};
 
 const BookList = () => {
+  const [selected, setSelected] = useState(null);
   const { loading, error, data } = useQuery(getBooksQuery);
-  if (loading) return <p>Loading books...</p>;
-  if (error) return <p>Error</p>;
-  console.log(data);
-
+  const selectBook = (id) => setSelected(id);
   return (
     <div>
       <ul id='book-list'>
-        {data.books.map((book) => (
-          <li key={book.id}>{book.name}</li>
-        ))}
+        <DisplayBooks
+          loading={loading}
+          error={error}
+          data={data}
+          selectBook={selectBook}
+        />
       </ul>
-      {/* OR with 'return':
-      <ul id='book-list'>
-        {data.books.map((book) => {
-          return <li key={book.id}> {book.name}</li>;
-        })}
-      </ul> */}
-      <BookDetails />
+      <BookDetails bookId={selected} />
     </div>
   );
 };
-
-// STEP 2: Bind the query to this component so that we can access the data that comes back from the query.
-// To do this, we use 'graphql' (which we import up above - this was installed when we installed Apollo) -
-// this helps us bind Apollo to React.
-// 'graphql' is a function. So, as a parameter, it takes in the query we want to bind to this component, in this case, our query, 'getBooksQuery'.
-// So basically, we are using 'graphql' to bind our 'getBooksQuery' to our 'BookList' component,
-// So that now, inside this component, we have access to all of the data that comes back from this query.
-// And this data is stored in the component's props.
-// export default graphql(getBooksQuery)(BookList);
 
 export default BookList;
